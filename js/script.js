6c27803dcd0477fb6d1334aa548cd83fd5e48202
Vi-shub <smsharma3121@gmail.com>
2024-08-04 23:22:16 +0530

    kardo

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-const carouselInner = document.querySelector('.custom-carousel-inner');
-const items = document.querySelectorAll('.custom-carousel-item');
-let currentIndex = 0;
-const totalItems = items.length;
-
-function showNextSlide() {
-    currentIndex = (currentIndex + 1) % totalItems;
-    const offset = -currentIndex * 100;
-    carouselInner.style.transform = `translateX(${offset}%)`;
-}
-
-setInterval(showNextSlide, 3000); // Change slide every 3 seconds
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const logosSlide = document.querySelector('.logos-slide');
     const logos = logosSlide.innerHTML;
